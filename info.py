42a875b6367a5893722d95bf4ae19c191b79db4a
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-06-26 00:11:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "seas
 NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
-    ON_HEROKU = True
+    ON_HEROKU = False
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
